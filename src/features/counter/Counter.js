c7ce6b48856d297214dda5dc33b855519d8d6508
권순성 <tnstjd120@gmail.com>
2022-09-10 20:56:25 +0900

    RTK + msw axios put

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './counterSlice'
+import { decrement, fetchIncrement, increment, incrementByAmount } from './counterSlice'
 // import styles from './Counter.module.css'
 
 export function Counter() {
@@ -11,8 +11,11 @@ export function Counter() {
         <div>
             <div>
                 <button aria-label="Increment value" onClick={() => dispatch(increment())}>Increment</button>
+                <button onClick={() => dispatch(fetchIncrement(count))}>fetchIncrement</button>
                 <span>{count}</span>
                 <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>Decrement</button>
+                <button onClick={() => dispatch(incrementByAmount(10))}>+10</button>
+                <button onClick={() => dispatch(incrementByAmount(-10))}>-10</button>
             </div>
         </div>
     )
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,6 +1,23 @@
 import { rest } from 'msw'
 
 export const handlers = [
+    rest.put("http://localhost:3000/counter/increment", async(req, res, ctx) => {
+        const { value } = req.body;
+        return res(
+            ctx.json({
+                value: value + 2
+            })
+        )
+    }),
+    rest.get("/login", async(req, res, ctx) => {
+        return res(
+            ctx.json({
+                id: "",
+                firstName: "John",
+                lastName: "Maverick"
+            })
+        )
+    }),
     rest.get('https://raw.githubusercontent.com/techoi/raw-data-api/main/simple-api.json', (req, res, ctx) => {
         return res(
             ctx.json({
