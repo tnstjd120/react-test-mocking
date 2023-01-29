ce50a30892d065f681a3352d117a00aa18b944bc
권순성 <tnstjd120@gmail.com>
2023-01-29 15:19:31 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-// import TestMocking from './components/TestMocking/TestMocking';
+import TestMocking from './components/TestMocking/TestMocking';
 import { Counter } from './features/counter/Counter'
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <TestMocking />
     </div>
   );
 }
